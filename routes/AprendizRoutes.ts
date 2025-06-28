ca02212dc15371a15fd5aa861bdfc69aae57157e
Diego336s <sanabriadiego336@gmail.com>
2025-06-27 22:55:29 -0500

    Rutas de ingreso creadas

--- routes/AprendizRoutes.ts
+++ routes/AprendizRoutes.ts
@@ -1,1 +1,12 @@
-El oscar melo mama
+import { Router } from "../dependencies/dependecias.ts";
+import { getAprendiz, postAprendiz, putAprendiz, deleteAprendiz } from "../controller/AprendizController.ts";
+import { Application } from 'https://deno.land/x/oak@v17.1.4/mod.ts';
+
+const aprendizRouter = new Router();
+
+aprendizRouter.get("/aprendiz", getAprendiz);
+aprendizRouter.post("/aprendiz", postAprendiz);
+aprendizRouter.put("/aprendiz", putAprendiz);
+aprendizRouter.delete("/aprendiz/:id", deleteAprendiz);
+
+export {aprendizRouter};
