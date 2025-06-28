b7c8795882a6743340dc048521a2bdf72bad04f1
Diego336s <sanabriadiego336@gmail.com>
2025-06-27 22:47:20 -0500

    metodos creados aprendiz

--- controller/AprendizController.ts
+++ controller/AprendizController.ts
@@ -1,1 +1,17 @@
 import { Context, RouterContext, z } from "../dependencies/dependecias.ts";
+
+export const getAprendiz = async (ctx: Context) =>{
+
+}
+
+export const postAprendiz = async (ctx: Context)=>{
+
+}
+
+export const putAprendiz = async (ctx: Context)=>{
+
+}
+
+export const deleteAprendiz = async (ctx: RouterContext<"/aprendiz/:id">)=>{
+
+}
