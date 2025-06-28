bc82ddcf75cfccf90902dd98547a32a333316339
Oscar30 <oxcarbohorfo13@gmail.com>
2025-06-27 22:50:20 -0500

    Metodos Instructor controller

---
+++ controller/InstructorController.ts
@@ -0,0 +1,6 @@
+import { Context, RouterContext, z } from "../dependencies/dependecias.ts";
+
+export const getInstructor = async (ctx: Context) => {};
+export const postInstructor = async (ctx: Context) => {};
+export const putInstructor = async (ctx: Context) => {};
+export const deleteInstructor = async (ctx: Context) => {};
--- routes/InstructorRouter.ts
+++ routes/InstructorRouter.ts
@@ -1,5 +1,11 @@
 import { Router } from "../dependencies/dependecias.ts";
+import { getInstructor } from "../controller/InstructorController.ts";
 
 const InstructorRouter = new Router();
 
-InstructorRouter.get("/instructor", );
+InstructorRouter.get("/instructor", getInstructor);
+InstructorRouter.post("/instructor", getInstructor);
+InstructorRouter.put("/instructor", getInstructor);
+InstructorRouter.delete("/instructor", getInstructor);
+
+export { InstructorRouter };
